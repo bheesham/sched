23ec9721ce2be261a7b0c9dd9243739001b78a8a
bheesham <bheesham@kong.ccsl.carleton.ca>
2014-02-26 17:20:44 -0500

    Should now save the name and email on the backend
    	modified:   api/server.py

--- api/server.py
+++ api/server.py
@@ -3,27 +3,41 @@ import pymysql
 from bottle import route, request, static_file, run
 
 conn = pymysql.connect(host='127.0.0.1', user='root', passwd='thisisatest', db='sched')
-cur = conn.cursor()
 
 @route('/api/<path:path>', method='POST')
 def api(path='invalid'):
+  cur = conn.cursor()
   if path == 'get-date-hours':
     hours = [8, 9, 10, 11, 12, 13, 14, 15, 16]
     date = request.forms.get('date')
-    cur.execute('SELECT hour FROM appointments WHERE date = %s;', date)
-    for row in cur:
-      hours.remove(row)
+    cur.execute('SELECT hour FROM appointments WHERE date = %s;', (date))
+    results = cur.fetchall()
+    print(results)
+    cur.close()
     return json.dumps({'hours': hours})
   if path == 'save-appointment':
     date = request.forms.get('date')
-    hour = int(request.forms.get('hour'))
-    cur.execute('SELECT hour FROM appointments WHERE date = %s AND hour = %d', (date, hour))
+    hour = request.forms.get('hour')
+    ihour = int(hour)
+
+    name = request.forms.get('name')
+    email = request.forms.get('email')
+
+    if len(name) < 1 or len(email) < 1:
+      return json.dumps({'message': 'Need to specify a name AND an email address'})
+
+    ret = ''
+    cur.execute('SELECT hour FROM appointments WHERE date = %s AND hour = %s;', (date, ihour))
     if cur.rowcount == 0:
-      cur.execute('INSERT INTO appointments (date, hour) VALUES (%s, %d);', date, hour)
-      return json.dumps({'message': 'Appointment has been booked'})
-    return json.dumps({'message': 'There has been an error while trying to book that time, please select another.'})
-  elif path == 'save-appointment':
-    return json.dumps({'message': 'not implemented'})
+      cur.execute('INSERT INTO appointments (date, hour, name, email) VALUES (%s, %s, %s, %s);', (date, ihour, name, email))
+      if cur.rowcount == 0:
+        ret = json.dumps({'message': 'There has been an error. Please email bheesham@ccsl.carleton.ca'})
+      else:
+        ret = json.dumps({'message': 'Appointment has been booked'})
+    else:
+      ret = json.dumps({'message': 'There has been an error while trying to book that time, please select another.'})
+    cur.close()
+    return ret
   else:
     return json.dumps({'error': 'invalid request'})
 
